59eb10242d07f530733e51f6070421b61e2125e3
Benjamin Livingstone <you@example.com>
2021-06-18 20:31:05 -0400

    Usuarios con cuentas bancarias v1

--- usuariosconcuentasbancarias.py
+++ usuariosconcuentasbancarias.py
@@ -11,6 +11,22 @@ class User:
         self.name = name
         self.email = email
         self.account = BankAccount(int_rate=0.02, balance=0)	# agregó esta línea
+    
+    def make_deposit(self,amount):
+        self.account.deposit(amount)
+
+    def make_withdrawal(self,amount):
+        self.account.withdraw(amount)
+    
+    def make_transer(self,amount):
+        self.account.transfer_money(amount)
+        
+    def new_account(self):
+        self.account.create()
+    
+    # def view_accoun_info():
+    #     self.account.
+
 # Nota: La clase BankAccount debe estar en el mismo archivo que la clase Usuario, por lo que se reconoce la referencia a la misma. Eche un vistazo a la modularización si siente la necesidad de tener las 2 clases en archivos separados.
 
 # Interactuamos con este nuevo atributo tal como lo hacemos con los atributos anteriores: ¡la única diferencia es que hemos definido personalmente la funcionalidad de esta clase! Conocemos los atributos y métodos disponibles para el atributo account mirando nuestra clase BankAccount.
@@ -21,35 +37,58 @@ class User:
 #     	print(self.account.balance)		# o acceder a sus atributos
 
 class BankAccount:
-    def __init__(self, int_rate=0, balance=0):
+    def __init__(self, int_rate=0, balance=0,accounts):
         self.int_rate = int_rate
         self.balance = balance
-        self.interes=0
+        self.accounts={}
+        self.interes=0        
     # def __str__():
+        return f"No ha ingresado todos los datos necesarios"
         
     def deposit(self, amount):
         self.balance += amount
-        return self
-        
-    def withdraw(self, amount):
-        self.balance -=amount
-        return self
-        
-    def display_account_info(self):
-        print(f"La taza de interes es del: {self.int_rate}% y los intereses acumulados son: ${self.interes}, el saldo final es: ${self.balance}")
+        # print(f"El {self.name} {self.account_balance}")
+        # print (self.account_balance)
         return self
 
-    def yield_interest(self):
-        if (self.balance>0):
-            self.interes=self.balance*self.int_rate
+    def withdraw(self, amount):
+        if amount>self.balance:
+            print(f"No puede hacer un retiro de {amount}, su saldo es de {self.balance}")
             return self
         else:
-            print(f"A trabajar")
+            self.balance -=amount
             return self
+        
+    def display_account_info(self):
+        print(f"La taza de interes es del: {self.int_rate} y los intereses acumulados son: ${self.interes}, el saldo final es: ${self.balance}")
+        return self
+
+    def transfer_money (self, other_user, amount):    
+        if amount>self.balance:
+            print(f"No puede hacer una transferencia de ${amount}, su saldo es de ${self.balance}")
+            return self        
+        else:
+            self.withdrawal(amount)
+            other_user.deposit(amount)
+            print(f"{self.name} hiso una transferencia de ${amount} a {other_user.name}")
+            return self
+    # def create(self):
+    #     account=
 
 Benjamin = User("Benjamin Livingstone","benjaminlivingstone@gmail.com")
-cuenta1=BankAccount(0.07,200)
-cuenta2=BankAccount(0.05,500)
+# Benjamin.account=BankAccount(0.07,200)
+# Benjamin.account2=BankAccount(0.05,500)
 
-Benjamin.account.deposit(100)		# podemos llamar los métodos de la instancia BankAccount 
-Benjamin.account.display_account_info()
+# Benjamin.account.deposit(100)		# podemos llamar los métodos de la instancia BankAccount 
+# Benjamin.account.yield_interest()
+# Benjamin.account.display_account_info()
+# print(Benjamin.account.balance)		# o acceder a sus atributos
+
+# Benjamin.account2.deposit(200)
+# #self.account.deposit(100)	
+# Benjamin.account2.yield_interest()
+# Benjamin.account2.display_account_info()
+
+Benjamin.make_deposit(100)
+
+# print(Benjamin.account.balance)
